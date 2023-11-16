8317190e231916ab3dd13bbc66cfe835b08972b1
HideInDevelopment <hideindevelopment96@gmail.com>
2023-11-16 19:59:17 +0100

    First commit with existing folder structure

---
+++ setup/test.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace setup
+{
+    public class test {
+        Console.WriteLine("Hola mundo");
+     }
+}
